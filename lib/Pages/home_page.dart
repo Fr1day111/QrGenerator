30523ccc586f0c7236be1f22b97a56fae71d2feb
Fri Day <nwrsudeep@gmail.com>
2024-03-06 15:32:06 +0545

    Little Updates

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,11 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qrgenerator/Providers/providers.dart';
+import 'package:qrgenerator/widgets/button.dart';
+import 'package:qrgenerator/widgets/text_field.dart';
+import 'package:qrgenerator/widgets/theme_changer.dart';
 
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends ConsumerStatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  ConsumerState<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends ConsumerState<MyHomePage> {
+  final textController= TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  String? link;
+  bool isSubmitted=false;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    bool isLoading = ref.watch(loadingProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Qr Generator'),
+        actions: const [
+          ThemeChangerButton()
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20,),
+                  MyTextField(label: 'Paste Your Link Here..', controller: textController),
+                  const SizedBox(height: 20,),
+                  !isLoading?MyButton(label: 'Submit', onTap: (){
+                    if(formKey.currentState!.validate()){
+                      bool _validURL = Uri.parse(textController.text).isAbsolute;
+                      if(_validURL){
+                        ref.read(loadingProvider.notifier).update((state) => true);
+                        Future.delayed(const Duration(seconds: 2)).then((value){
+                          link = textController.text;
+                          isSubmitted =true;
+                          ref.read(loadingProvider.notifier).update((state) => false);
+                        });
+                      }
+                    }
+                  }):const CircularProgressIndicator(),
+                  isSubmitted?QrImageView(data: link!,size: 200,backgroundColor: Colors.white,):Container()
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/text_field.dart
+++ lib/widgets/text_field.dart
@@ -2,46 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class MyTextField extends StatelessWidget {
-  const MyTextField({required this.label, required this.controller,this.isPassword,this.maxLines,this.isNumber,this.isAutoValidate, this.isMultiline, this.isEmail, super.key});
+  const MyTextField({required this.label, required this.controller,this.isAutoValidate, super.key});
 
   final String label;
   final TextEditingController controller;
-  final bool? isPassword;
-  final int? maxLines;
-  final bool? isNumber;
   final bool? isAutoValidate;
-  final bool? isMultiline;
-  final bool? isEmail;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      autovalidateMode: isAutoValidate==false?AutovalidateMode.disabled:AutovalidateMode.onUserInteraction,
       validator: (value){
-        if(isEmail==true){
-          bool emailValid = RegExp(
-              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-              .hasMatch(value!);
-          if (emailValid != true) {
-            return 'Email Format not matched';
-          }
-        }
-        if(value == null||value.isEmpty){
+        bool validURL = Uri.parse(value!).isAbsolute;
+        if(value.isEmpty){
           return 'This Field Cant be Empty';
+        }else if(!validURL){
+          return 'Enter the valid URL';
         }
         return null;
       },
       controller: controller,
-      maxLines: maxLines??1,
-      keyboardType: isNumber==true?TextInputType.number:isMultiline==true?TextInputType.multiline:TextInputType.text,
-      inputFormatters: isNumber==true?[
-        FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d*')),
-      ]:[],
       decoration: InputDecoration(
           labelText: label,
           border: const OutlineInputBorder(
               borderRadius: BorderRadius.all(Radius.circular(10)))),
-      obscureText: isPassword??false,
     );
   }
 }
